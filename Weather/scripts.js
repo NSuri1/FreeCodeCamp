d3ed8454a41d9290193bca8815fa809a632f089a
Macbook <nalinsuri@gmail.com>
2016-12-26 20:59:32 -0500

    background setting template

--- Weather/scripts.js
+++ Weather/scripts.js
@@ -34,12 +34,12 @@ function success(location){
 	$.getJSON(jsonLocation, data, function(weather){
 		temperatureInKelvin = weather["main"]["temp"];
 		var temp = temperatureInKelvin;
-		var weatherMain = weather["weather"][0];
 		$(".location").text(weather["name"]);
-		$(".pic").attr("src", "http://openweathermap.org/img/w/" + weatherMain["icon"] + ".png");
-		
-		$(".temp").text(kToF(temperatureInKelvin) + "\xB0")
-		$(".description").html(weatherMain["description"]);
+		$(".description").html(weather["weather"][0]["description"].toUpperCase());
+		$(".temp").text(kToF(temperatureInKelvin) + "\xB0");
+
+		var id = weather["weather"][0]["id"];	
+		setBackground(id);
 	});
 }
 
@@ -54,4 +54,80 @@ function kToF(temp) {
 
 function kToC(temp){
 	return Math.round(temp - 273.15);
+}
+
+function setBackground(id){
+		if(200 <= id < 300){
+		} 
+		else if (300 <= id < 400){
+		}
+		else if (500 <= id < 600){
+		}
+		else if (600 <= id < 700){
+		}
+		else if (700 <= id < 800){
+		}
+		else if (800 <= id < 900){
+			switch(id){
+				case 800:
+				break;
+				case 801:
+				break;
+				case 802:
+				break;
+				case 803:
+				break;
+				case 804:
+				break;
+				default:
+			}
+		}
+		else if (900 <= id < 910){
+			switch(id){
+				case 900:
+				break;
+				case 901:
+				break;
+				case 902:
+				break;
+				case 903:
+				break;
+				case 904:
+				break;
+				case 905:
+				break;
+				case 906:
+				break;
+				default:
+			}
+		}	
+		else {
+			switch(id){
+				case 951:
+				break;
+				case 952:
+				break;
+				case 953:
+				break;
+				case 954:
+				break;
+				case 955:
+				break;
+				case 956:
+				break;
+				case 957:
+				break;
+				case 958:
+				break;
+				case 959:
+				break;
+				case 960:
+				break;
+				case 961:
+				break;
+				case 962:
+				break;
+				default:
+			}
+		}
 }
