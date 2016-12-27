e7a9140a0b81f7e73fbde253ac346214b880069c
Macbook <nalinsuri@gmail.com>
2016-12-26 21:52:15 -0500

    Add background image

--- Weather/scripts.js
+++ Weather/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		navigator.geolocation.getCurrentPosition(success);
 	}
 	else {
-		alert("Geolocation is not working");
+		alert("Geolocation is not working. \nPlease try again another time.");
 	}
 
 	$(".fahrenheit").attr("disabled", "disabled");
@@ -43,10 +43,10 @@ function success(location){
 	$.getJSON(jsonLocation, data, function(weather){
 		temperatureInKelvin = weather["main"]["temp"];
 		var temp = temperatureInKelvin;
-		var id = weather["weather"][0]["id"];	
+
 		$(".location").text(weather["name"]);
 		$(".description").html(weather["weather"][0]["description"].toUpperCase());
-		if(200 <= id < 900){
+		if(200 <= weather["weather"][0]["id"] < 900){
 			$(".pic").attr("src", "http://openweathermap.org/img/w/" + weather["weather"][0]["icon"] + ".png");
 			$(".pic").show();
 		}
@@ -55,7 +55,6 @@ function success(location){
 		}
 
 		$(".temp").text(kToF(temperatureInKelvin) + "\xB0F");
-		setBackground(id);
 	});
 }
 
@@ -65,80 +64,4 @@ function kToF(temp) {
 
 function kToC(temp){
 	return Math.round(temp - 273.15);
-}
-
-function setBackground(id){
-		if(200 <= id < 300){
-		} 
-		else if (300 <= id < 400){
-		}
-		else if (500 <= id < 600){
-		}
-		else if (600 <= id < 700){
-		}
-		else if (700 <= id < 800){
-		}
-		else if (800 <= id < 900){
-			switch(id){
-				case 800:
-				break;
-				case 801:
-				break;
-				case 802:
-				break;
-				case 803:
-				break;
-				case 804:
-				break;
-				default:
-			}
-		}
-		else if (900 <= id < 910){
-			switch(id){
-				case 900:
-				break;
-				case 901:
-				break;
-				case 902:
-				break;
-				case 903:
-				break;
-				case 904:
-				break;
-				case 905:
-				break;
-				case 906:
-				break;
-				default:
-			}
-		}	
-		else {
-			switch(id){
-				case 951:
-				break;
-				case 952:
-				break;
-				case 953:
-				break;
-				case 954:
-				break;
-				case 955:
-				break;
-				case 956:
-				break;
-				case 957:
-				break;
-				case 958:
-				break;
-				case 959:
-				break;
-				case 960:
-				break;
-				case 961:
-				break;
-				case 962:
-				break;
-				default:
-			}
-		}
 }
