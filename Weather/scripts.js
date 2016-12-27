7eaa192d1b30e7b5c81114e451d68cb21ecd10bf
Macbook <nalinsuri@gmail.com>
2016-12-26 19:36:07 -0500

    $.getJSON() working

--- Weather/scripts.js
+++ Weather/scripts.js
@@ -6,10 +6,27 @@ $(document).ready(function(){
 	
 });
 
+var temp = 0;
+
 function success(location){
-	console.log("success");
+	var lat = location.coords.latitude;
+	var long = location.coords.longitude;
+	var jsonLocation = "http://api.openweathermap.org/data/2.5/weather";
+	var data = {
+		"lat": lat,
+		"lon": long,
+		"APPID": "e34a9855fd2a7c8af5f930144984b5c1"
+	}
+	$.getJSON(jsonLocation, data, function(weather){
+		temp = weather["main"]["temp"];
+		$(".location").text(weather["name"]);
+		$(".pic").attr("src", "http://openweathermap.org/img/w/" + weather["weather"]["icon"] + ".png");
+		$(".temp").text(temp + "\xB0")
+		$(".description").html(weather["weather"]["description"]);
+	});
 }
 
 function error(msg){
-	alert("Error: " + msg);
+	alert("Error in getting current location. \nMiami's weather will be displayed");
+	$(".location").text("Miami, FL");
 }
