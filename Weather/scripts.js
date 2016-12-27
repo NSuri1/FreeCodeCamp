3e6d8a6a22e11f70af5ca6a49d3bd61b10422e1a
Macbook <nalinsuri@gmail.com>
2016-12-26 20:31:58 -0500

    temperature conversions

--- Weather/scripts.js
+++ Weather/scripts.js
@@ -3,10 +3,23 @@ $(document).ready(function(){
 		navigator.geolocation.getCurrentPosition(success, error);
 	}
 
+	$(".fahrenheit").attr("disabled", "disabled");
+
+	$(".celcius").on("click", function(){
+		$(".celcius").attr("disabled", "disabled");
+		$(".fahrenheit").removeAttr("disabled");
+		$(".temp").text(kToC(temperatureInKelvin) + "\xB0");
+	});
+
+	$(".fahrenheit").on("click", function(){
+		$(".fahrenheit").attr("disabled", "disabled");
+		$(".celcius").removeAttr("disabled");
+		$(".temp").text(kToF(temperatureInKelvin) + "\xB0");
+	});
 	
 });
 
-var temp = 0;
+var temperatureInKelvin = 0;
 
 function success(location){
 	var lat = location.coords.latitude;
@@ -17,16 +30,28 @@ function success(location){
 		"lon": long,
 		"APPID": "e34a9855fd2a7c8af5f930144984b5c1"
 	}
+
 	$.getJSON(jsonLocation, data, function(weather){
-		temp = weather["main"]["temp"];
+		temperatureInKelvin = weather["main"]["temp"];
+		var temp = temperatureInKelvin;
+		var weatherMain = weather["weather"][0];
 		$(".location").text(weather["name"]);
-		$(".pic").attr("src", "http://openweathermap.org/img/w/" + weather["weather"]["icon"] + ".png");
-		$(".temp").text(temp + "\xB0")
-		$(".description").html(weather["weather"]["description"]);
+		$(".pic").attr("src", "http://openweathermap.org/img/w/" + weatherMain["icon"] + ".png");
+		
+		$(".temp").text(kToF(temperatureInKelvin) + "\xB0")
+		$(".description").html(weatherMain["description"]);
 	});
 }
 
 function error(msg){
 	alert("Error in getting current location. \nMiami's weather will be displayed");
 	$(".location").text("Miami, FL");
+}
+
+function kToF(temp) {
+	return Math.round((temp * 9 / 5) - 459.67);
+}
+
+function kToC(temp){
+	return Math.round(temp - 273.15);
 }
