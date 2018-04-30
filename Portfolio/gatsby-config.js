c2b8ef8e3bccbcd3fe955169fab50a2e2a7a0c01
Macbook <nalinsuri@gmail.com>
2018-04-29 21:13:19 -0400

    Update links

---
+++ Portfolio/gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `open sans\:300,600`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "<your-tracking-id-here>",
+        head: true
+      }
+    },
+  ],
+  siteMetadata: {
+    title: 'Nalin Suri',
+    description: '',
+    keywords: 'personal website',
+    url: 'http://www.suriosity.com'
+  }
+};
